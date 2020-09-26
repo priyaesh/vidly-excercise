6a10dfba2b164f12ccd3913693e2c701315d3664
hemapriya <hemapriyasampath@gmail.com>
2020-09-25 20:20:12 -0400

    Extracting movies Table

---
+++ src/common/moviesTable.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+const MoviesTable = (props) => {
+  const { movies, onDelete } = props;
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Genre</th>
+          <th>Stock</th>
+          <th>Rate</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.movies.map((movie) => (
+          <tr key={movie._id}>
+            <td>{movie.title}</td>
+            <td>{movie.genre.name}</td>
+            <td>{movie.numberInStock}</td>
+            <td>{movie.dailyRentalRate}</td>
+            <td>
+              <button
+                onClick={() => onDelete(movie)}
+                className="btn btn-danger btn-sm"
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default MoviesTable;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,6 +6,7 @@ import { paginate } from "../utils/paginate";
 import { getMovies } from "../fakeMovieService";
 import { getGenres } from "../fakeGenreService";
 import { get } from "lodash";
+import MoviesTable from "../common/moviesTable";
 
 class Movies extends Component {
   state = {
@@ -19,7 +20,7 @@ class Movies extends Component {
     const genre = [{ _id: "0", name: "All Genres" }, ...getGenres()];
     this.setState({ movies: getMovies(), genre });
   }
-  handleOnClick = (movie) => {
+  handleOnDelete = (movie) => {
     console.log("Button Clicked", movie);
     const movies = this.state.movies.filter((m) => m._id !== movie._id);
     this.setState({ movies: movies });
@@ -61,35 +62,7 @@ class Movies extends Component {
         </div>
         <div className="col">
           <p>There are {filtered.length} in the database</p>
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Genre</th>
-                <th>Stock</th>
-                <th>Rate</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {movies.map((movie) => (
-                <tr key={movie._id}>
-                  <td>{movie.title}</td>
-                  <td>{movie.genre.name}</td>
-                  <td>{movie.numberInStock}</td>
-                  <td>{movie.dailyRentalRate}</td>
-                  <td>
-                    <button
-                      onClick={() => this.handleOnClick(movie)}
-                      className="btn btn-danger btn-sm"
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <MoviesTable movies={movies} onDelete={this.handleOnDelete} />
           <Pagination
             itemCount={filtered.length}
             pageSize={pageSize}
